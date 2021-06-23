ae7cb14fdc7f1060037e460a7c470a6cbd49fa91
Russels-teapot <baxarev.777@yandex.ru>
2021-06-23 22:03:51 +0300

    Выполнена домашняя работа

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,15 +1,23 @@
 import compare from '../app';
+const arr = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'артиллерист', health: 80 },
+];
+const sortedArr = arr.sort(compare);
 
 test('check status', () => {
-  const arr = [
-    { name: 'мечник', health: 10 },
+  expect(sortedArr).toEqual([
     { name: 'маг', health: 100 },
     { name: 'лучник', health: 80 },
     { name: 'артиллерист', health: 80 },
-  ];
-  const sortedArr = arr.sort(compare);
+    { name: 'мечник', health: 10 },
+  ]);
+});
 
-  expect(sortedArr).toEqual([
+test('check toBe', () => {
+  expect(sortedArr).not.toBe([
     { name: 'маг', health: 100 },
     { name: 'лучник', health: 80 },
     { name: 'артиллерист', health: 80 },
